0d7189b9f77b5a10441c7126be53b3a1b47059ae
faizakmalz <faztcollege@gmail.com>
2025-01-18 19:28:34 +0700

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,8 @@
 
 @section('title', 'Dashboard')
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+        
         <title>Laravel</title>
 
         <!-- Fonts -->
